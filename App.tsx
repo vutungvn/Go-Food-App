4b7fa14e9b1cc7ace15cd248dfd2a38a8560976d
Thanh Tung <tungvuvanthanh@gmail.com>
2025-01-17 10:13:49 +0700

    Video 28

--- App.tsx
+++ App.tsx
@@ -1,16 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello world with Thanh Tung bla bla</Text>
-      <StatusBar style="auto" />
+      <View>
+        <Text style={styles.text}>test</Text>
+      </View>
+      <Text style={styles.text}>Hello world with
+        <Text style={styles.thanhtung}> Thanh Tung</Text>
+      </Text>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  thanhtung: {
+    color: "green"
+  },
+
+  text: {
+    fontSize: 30,
+    color: "red"
+  },
+
   container: {
     flex: 1,
     backgroundColor: '#fff',
