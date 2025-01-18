82f15fcbb34d2b4bfa2b39c5327a397487f175aa
Thanh Tung <tungvuvanthanh@gmail.com>
2025-01-18 09:54:16 +0700

    video29

--- App.tsx
+++ App.tsx
@@ -1,20 +1,38 @@
+import { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+
+  //data-type
+  //string
+  const [name, setName] = useState<string>("hoidanit");
+
+  //number
+  const [age, setAge] = useState<number>(30);
+
+  //null, undefined, boolean
+  const test = false;
+
+  //object, array
+  const [person, setPerson] = useState([6, 9]);
+
+
+
   return (
     <View style={styles.container}>
       <View>
-        <Text style={styles.text}>test</Text>
+        <Text style={styles.text}>{name}</Text>
+        <Text style={styles.text}>{JSON.stringify(person)}</Text>
       </View>
       <Text style={styles.text}>Hello world with
-        <Text style={styles.thanhtung}> Thanh Tung</Text>
+        <Text style={styles.hoidanit}> hoidanit</Text>
       </Text>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  thanhtung: {
+  hoidanit: {
     color: "green"
   },
 
@@ -26,7 +44,9 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    // alignItems: 'center',
+    // justifyContent: 'center',
+    paddingTop: 20,
+    paddingHorizontal: 20
   },
 });
