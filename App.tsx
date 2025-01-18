f8bf837bb66d139b99eb34da1875acf20f3e912d
Thanh Tung <tungvuvanthanh@gmail.com>
2025-01-18 17:38:19 +0700

    video 31

--- App.tsx
+++ App.tsx
@@ -5,6 +5,19 @@ export default function App() {
 
   const [name, setName] = useState<string>("");
 
+  const [todoList, setTodoList] = useState([
+    { id: 1, title: "Learn React Native" },
+    { id: 2, title: "Learn React.js" },
+    { id: 3, title: "Watching Netflix" },
+    { id: 4, title: "Playing ESport" },
+    { id: 5, title: "Subscribe Hỏi Dân IT :v" },
+    { id: 6, title: "Watching Youtube" },
+    { id: 7, title: "CR 7" },
+    { id: 8, title: "Tony Kroos" },
+    { id: 9, title: "Nine" },
+    { id: 10, title: "M10" },
+  ])
+
   return (
     <View style={styles.container}>
       <View>
@@ -24,16 +37,30 @@ export default function App() {
         <Text style={styles.text}>{name}</Text>
       </View>
 
-      <Button title='Add new' />
+      <Button
+        title='Add new'
+        onPress={() => alert("tap me")}
+      />
 
-      <Text style={styles.text}>Hello world with
-        <Text style={styles.hoidanit}> hoidanit</Text>
-      </Text>
+      <View style={{ marginTop: 20, borderColor: "red", borderWidth: 1 }}>
+        {todoList.map(todo => {
+          return (
+            <Text style={styles.todo}>{todo.title}</Text>
+          )
+        })}
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  todo: {
+    fontSize: 30,
+    backgroundColor: "pink",
+    marginBottom: 20,
+    padding: 15
+  },
+
   hoidanit: {
     color: "green"
   },
@@ -49,6 +76,7 @@ const styles = StyleSheet.create({
     // alignItems: 'center',
     // justifyContent: 'center',
     paddingTop: 20,
-    paddingHorizontal: 20
+    paddingHorizontal: 20,
+    marginTop: 50
   },
 });
